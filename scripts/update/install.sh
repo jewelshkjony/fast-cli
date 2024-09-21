97dc05965e09c3b8281a921f22644cc51d5aab8e
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-09-21 14:07:51 +0600

    Update install.sh

--- scripts/update/install.sh
+++ scripts/update/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Base64-encoded URL for the ZIP file
-zipBase="aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC91cGRhdGUvdXBkYXRlLnppcA=="
+zipBase="aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9FeHRlbnNpb25zL3JlbGVhc2VzL2Rvd25sb2FkL3YxLjAuNC91cGRhdGUuemlw"
 zipUrl=$(echo "$zipBase" | base64 --decode)
 
 # Define the location to store the ZIP file and the destination directory
@@ -31,4 +31,4 @@ unzip -o "$zipLocation" -d "$destinationDir"
 # Remove the downloaded ZIP file
 rm "$zipLocation"
 
-echo "Fast has been successfully updated!"
+echo "Fast has been successfully updated to v1.0.4!"
