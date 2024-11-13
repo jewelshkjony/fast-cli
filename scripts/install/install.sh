d4f13b306431d108371e01de05ed7493f2351bda
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-11-13 15:38:09 +0600

    Updated to v1.2.8 13.11.24.15.09

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -2,7 +2,7 @@
 
 # Installation script for Linux, MacOS, and Android Termux
 # Base64-encoded URL for the ZIP file
-zipBase="aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92MS4yLjcvZmFzdC56aXA="
+zipBase="aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92MS4yLjgvZmFzdC56aXA="
 zipUrl=$(echo "$zipBase" | base64 --decode)
 
 # Check if FAST_HOME environment variable exists and use it, otherwise fallback to $HOME/.local/share/Fast
@@ -80,4 +80,4 @@ if [ -f "$HOME/.zshrc" ]; then
     source "$HOME/.zshrc"
 fi
 
-echo "Fast-v1.2.7 12.11.24.21.50 has been successfully installed."
+echo "Fast-v1.2.8 13.11.24.15.09 has been successfully installed."
