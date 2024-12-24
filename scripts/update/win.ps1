4a8b4b4d674927d27cb125eb0bab47a0936e8a6d
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-12-24 12:44:05 +0600

    Updated to v2.0.9 24.12.24.10.35

--- scripts/update/win.ps1
+++ scripts/update/win.ps1
@@ -1,6 +1,6 @@
 # Update scripts for Windows
 # Base64-encoded URL for the ZIP file
-$zipBase = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92Mi4wLjgvdXBkYXRlLnppcA=="
+$zipBase = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92Mi4wLjkvdXBkYXRlLnppcA=="
 $zipBytes = [System.Convert]::FromBase64String($zipBase)
 $zipUrl = [System.Text.Encoding]::UTF8.GetString($zipBytes)
 
@@ -46,4 +46,4 @@ else {
 # Remove the downloaded ZIP file
 Remove-Item $zipLocation
 
-Write-Output "Fast has been successfully updated to v2.0.8 23.12.24.20.59"
+Write-Output "Fast has been successfully updated to v2.0.9 24.12.24.10.35"
