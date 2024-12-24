781fba4e5324ce636cf1417cc896ef664a7f6964
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-12-24 12:48:04 +0600

    Updated to v2.0.9 24.12.24.10.35

--- scripts/install/win.ps1
+++ scripts/install/win.ps1
@@ -1,5 +1,5 @@
 # Scripts to install Fast CLI for Windows
-$zipBase = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92Mi4wLjgvZmFzdC56aXA="
+$zipBase = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9mYXN0LWNsaS9yZWxlYXNlcy9kb3dubG9hZC92Mi4wLjkvZmFzdC56aXA="
 $zipBytes = [System.Convert]::FromBase64String($zipBase)
 $zipUrl = [System.Text.Encoding]::UTF8.GetString($zipBytes)
 
@@ -52,4 +52,4 @@ if (!(";$Path;".ToLower() -like "*;$destinationDir;*".ToLower())) {
     $Env:Path += ";$destinationDir"
 }
 
-Write-Output "Fast-v2.0.8 23.12.24.20.59 has been successfully installed."
+Write-Output "Fast-v2.0.9 24.12.24.10.35 has been successfully installed."
