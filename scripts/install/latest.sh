d49adb87061050b49c64d7201dbd5e8d2482e9c0
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2025-01-20 07:58:18 +0600

    Update latest.sh

--- scripts/install/latest.sh
+++ scripts/install/latest.sh
@@ -8,11 +8,11 @@ response=$(curl -s "$apiUrl")
 
 # Check if the API call was successful
 if [ $? -ne 0 ] || [ -z "$response" ]; then
-    echo "Failed to fetch data from the GitHub API. Check your internet connection or the API URL."
+    echo "Failed to fetch data from the GitHub API. Check your internet connection."
     exit 1
 fi
 
-# Extract the URL for fast.zip using grep and sed
+# Extract the URL using grep and sed
 zipUrl=$(echo "$response" | grep -o '"browser_download_url": *"[^"]*fast.zip"' | sed 's/"browser_download_url": *"//;s/"$//')
 
 # Check if the URL was found
@@ -20,6 +20,18 @@ if [ -z "$zipUrl" ]; then
     echo "fast.zip not found in the release assets."
     exit 1
 fi
+
+# Extract the tag_name value
+tagName=$(echo "$response" | grep -o '"tag_name": *"[^"]*"' | sed 's/"tag_name": *"//;s/"$//')
+
+# Check if tag_name was found
+if [ -z "$tagName" ]; then
+    echo "tag_name not found in the response."
+    exit 1
+fi
+
+# Output the tag_name
+echo "Tag Name: $tagName"
 
 # Output the URL
 echo "Fast Download URL: $zipUrl"
